31dbdb4d975e8f33d53fa4f2743d5e083dc06486
vidhi shah <vidhishah4480@gmail.com>
2023-08-14 20:30:49 +0530

    Called API Services

---
+++ lib/apicalls.dart
@@ -0,0 +1,113 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:vocalpalette/apikey.dart';
+class apicall{
+  final List<Map<String,String>>messages=[];
+  Future<String>isart(String prompt)async{
+    try {
+final res=await http.post(Uri.parse('https://api.openai.com/v1/chat/completions'),
+headers: {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer $apikey'
+},
+body: jsonEncode({
+  "model": "gpt-3.5-turbo",
+  "messages": [
+  {
+    "role": "user",
+    "content": "Does this prompt want to generate AI image,Arto or anything similar?$prompt,Simply answer this In yes or no"
+  },]
+}));
+print(res.body);
+if(res.statusCode==200){
+ String content=jsonDecode(res.body)['choices'][0]['message']['content'];
+ content=content.trim();
+
+ switch (content) {
+   case 'Yes':
+   case 'yes':
+   case 'Yes.':
+   case 'yes.':
+     final res = await dalle(prompt);
+     return res;
+   default:
+     print(content);
+     final res = await chatgpt(prompt);
+     return res;
+ }
+
+}return 'an internal error occured';
+    }
+    catch(e){
+return e.toString();
+    }
+  }
+  Future<String>chatgpt(String prompt)async{
+    messages.add({
+      "role": "user",
+      "content":'$prompt'
+    });
+    try {
+      final res=await http.post(Uri.parse('https://api.openai.com/v1/chat/completions'),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $apikey'
+          },
+          body: jsonEncode({
+            "model": "gpt-3.5-turbo",
+            "messages":
+             messages}));
+
+      if(res.statusCode==200){
+        String content=jsonDecode(res.body)['choices'][0]['message']['content'];
+        content=content.trim();
+       messages.add({
+      "role": "system",
+      "content":'$content'
+       });
+       return content;
+      }
+      return 'an internal error occured';
+
+
+    }
+    catch(e){
+      return e.toString();
+    }
+  }
+  Future<String>dalle(String prompt)async{
+    messages.add({
+      "role": "user",
+      "content":'$prompt'
+    });
+    try {
+      final res=await http.post(Uri.parse('https://api.openai.com/v1/images/generations'),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $apikey'
+          },
+          body: jsonEncode({
+           'prompt':prompt,
+          'n':1}));
+
+      if(res.statusCode==200){
+        String image=jsonDecode(res.body)["data"][0]["url"];
+        image=image.trim();
+        messages.add({
+          "role": "system",
+          "content":'$image'
+        });
+        return image;
+      }
+      return 'an internal error occured';
+
+
+    }
+    catch(e){
+      return e.toString();
+    }
+  }
+
+
+}
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,11 @@
+
+import 'package:http/http.dart';
 import 'package:flutter/material.dart';
-import 'package:vocalpalette/pallete.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:vocalpalette/apicalls.dart';
 import 'features.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+
 class homepage extends StatefulWidget {
   const homepage({Key? key}) : super(key: key);
 
@@ -9,7 +14,42 @@ class homepage extends StatefulWidget {
 }
 
 class _homepageState extends State<homepage> {
+  final speechtotext = SpeechToText();
+final apicall apiservice=apicall();
+   String lastWords='';
   @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechtotext.initialize();
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechtotext.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechtotext.stop();
+    setState(() {});
+  }
+  // SpeechRecognitionResult
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+      print(lastWords);
+    });
+  }
+  @override
+  void dispose(){
+    super.dispose();
+    speechtotext.stop();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -20,77 +60,87 @@ class _homepageState extends State<homepage> {
       ),
       body: Column(
         children: [
-          Stack(
-            children: [Center(
+          Stack(children: [
+            Center(
               child: Container(
                 height: 120.0,
                 width: 120.0,
                 margin: EdgeInsets.only(top: 4.0),
                 decoration: BoxDecoration(
-                  color: Color.fromRGBO(209, 243, 249, 1),
-                  shape: BoxShape.circle
-                ),
-
+                    color: Color.fromRGBO(209, 243, 249, 1),
+                    shape: BoxShape.circle),
               ),
-
             ),
-              Container(
-                height: 127.0,
-                decoration: BoxDecoration(
+            Container(
+              height: 127.0,
+              decoration: BoxDecoration(
                   shape: BoxShape.circle,
                   image: DecorationImage(
-                    image: AssetImage('assets/images/main.png')
-                  )
-                ),
-              )
+                      image: AssetImage('assets/images/main.png'))),
+            )
           ]),
           Container(
-            padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
-            margin: EdgeInsets.symmetric(horizontal: 37.0,).copyWith(top: 20.0),
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            margin: EdgeInsets.symmetric(
+              horizontal: 37.0,
+            ).copyWith(top: 20.0),
             decoration: BoxDecoration(
-              border: Border.all(
-                color: Color.fromRGBO(200, 200, 200, 1)
-              ),
-              borderRadius: BorderRadius.circular(20.0).copyWith(topLeft:Radius.zero)
-            ),
+                border: Border.all(color: Color.fromRGBO(200, 200, 200, 1)),
+                borderRadius:
+                    BorderRadius.circular(20.0).copyWith(topLeft: Radius.zero)),
             child: Padding(
               padding: const EdgeInsets.symmetric(vertical: 10.0),
               child: Text(
-                'HEY THERE 🙋🏻 How can i help you today?',style: TextStyle(
-                fontFamily: 'cera pro',
-                color: Color.fromRGBO(19, 61, 95, 1),
-                fontSize: 20.0
-
-              ),
+                'HEY THERE 🙋🏻 How can i help you today?',
+                style: TextStyle(
+                    fontFamily: 'cera pro',
+                    color: Color.fromRGBO(19, 61, 95, 1),
+                    fontSize: 20.0),
               ),
             ),
-          ),Padding(
+          ),
+          Padding(
             padding: const EdgeInsets.all(10.0),
-
             child: Container(
-              margin: EdgeInsets.only(top:15.0,left: 15.0),
+              margin: EdgeInsets.only(top: 15.0, left: 15.0),
               alignment: Alignment.centerLeft,
-              child: Text('Here are features you can use:',style: TextStyle(
-                  fontFamily: 'cera pro',
-                  color: Color.fromRGBO(19, 61, 95, 1),
-                  fontSize: 18.0,fontWeight: FontWeight.bold
-
-              ),),
+              child: Text(
+                'Here are features you can use:',
+                style: TextStyle(
+                    fontFamily: 'cera pro',
+                    color: Color.fromRGBO(19, 61, 95, 1),
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
           ),
           Column(
             children: [
-             features(Color.fromRGBO(165, 231, 244, 1), "ChatGpt","use it bhai easy peasy"),
-              features(Color.fromRGBO(157, 202, 235, 1), "Dell-E","bhai kya kamal ki cheez hai"),
-              features(Color.fromRGBO(162, 238, 239, 1), "Smart Voice Assistant", "Masti rukni nahi chahiye")
+              features(Color.fromRGBO(165, 231, 244, 1), "ChatGpt",
+                  "use it bhai easy peasy"),
+              features(Color.fromRGBO(157, 202, 235, 1), "Dell-E",
+                  "bhai kya kamal ki cheez hai"),
+              features(Color.fromRGBO(162, 238, 239, 1),
+                  "Smart Voice Assistant", "Masti rukni nahi chahiye")
             ],
           ),
-
         ],
-
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () async {
+          if(await speechtotext.hasPermission&&speechtotext.isNotListening){
+            await startListening();
+          }
+          else if(speechtotext.isListening){
+            final a=await apiservice.isart(lastWords);
+            print(a);
+            await stopListening();
+          }
+          else{
+            initSpeechToText();
+
+          }
+        },
         backgroundColor: Color.fromRGBO(165, 231, 244, 1),
         child: Icon(Icons.mic),
       ),
